67ba6f1560886253761bcdfc75036eb4411858d4
Tim Belko <tim.belko@gmail.com>
2014-12-30 20:21:59 -0500

    Committing all new files

---
+++ server.js
@@ -0,0 +1,29 @@
+// require the restify library.
+var restify = require('restify'),
+// create an HTTP server.
+server = restify.createServer();
+search = new (require('./lib').Search);
+// add the query-string parsing extension
+// to restify.
+server.use(restify.queryParser());
+// lookup packages by their name.
+server.get('/search', function (req, res, cb) {
+    search.search(req.params.q,
+    function (err, results) {
+        res.send(results);
+        cb();
+    });
+});
+// serve static JavaScript and CSS.
+server.get(/\/js|css|images\/?.*/, restify.serveStatic({
+    directory: './assets'
+}));
+
+ //add a route that listens on http://localhost:5000/hello/world
+server.get('/hello', function (req, res, cb) {
+    res.send("Hello World!");
+    return cb();
+});
+server.listen(process.env.PORT || 5000, function () { // bind server to port 5000.
+    console.log('%s listening at %s', server.name, server.url);
+});
